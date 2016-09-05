aa0ccadffd028d92954f1e2e32d4cf9a8ec0f19c
SpadesAce <lukasdewit@gmail.com>
2016-09-05 19:26:55 +0200

    added launcher without further functionality, last commit in master branch (hopefully)

---
+++ src/Launcher.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class is the main launcher of the game
+ * @author Lukas
+ *
+ */
+public class Launcher extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+}
