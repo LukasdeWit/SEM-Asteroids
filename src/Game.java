d65bedb797f9134711e17a25fb8822b7b111827a
Esmee Hildebrand <e.a.hildebrand@student.tudelft.nl>
2016-09-09 17:05:12 +0200

    fuck checkstyle

--- src/Game.java
+++ src/Game.java
@@ -4,160 +4,153 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Game {
-    private ArrayList<Entity> entities;
-    private ArrayList<Entity> destroyList;
-    private ArrayList<Entity> createList;
-    private ArrayList<Entity> addList;
-    private float screenX;
-    private float screenY;
-    private GraphicsContext gc;
-    private long restartTime;
-    private int score;
-    
-    private final double[] ScoreDisplayX={190,200,190,200,190,200}; //first one only
-    private final double[] ScoreDisplayY={ 20, 20, 30, 30, 40, 40};
+	private ArrayList<Entity> entities;
+	private ArrayList<Entity> destroyList;
+	private ArrayList<Entity> createList;
+	private ArrayList<Entity> addList;
+	private float screenX;
+	private float screenY;
+	private GraphicsContext gc;
+	private long restartTime;
+	private int score;
 
-    private final int[][] NumberLines={
-            {0,1,5,4,0},
-            {1,5},
-            {0,1,3,2,4,5},
-            {0,1,3,2,3,5,4},
-            {0,2,3,1,5},
-            {1,0,2,3,5,4},
-            {1,0,4,5,3,2},
-            {0,1,5},
-            {2,0,1,5,4,2,3},
-            {4,5,1,0,2,3}
-    };
-    
-    public Game(GraphicsContext gc) {
-        this.gc = gc;
-        screenX = 500;
-        screenY = 500;
-        entities = new ArrayList<Entity>();
-        addList = new ArrayList<Entity>();
-        destroyList = new ArrayList<Entity>();
-        createList = new ArrayList<Entity>();
-        startGame();
-    }
+	private final double[] ScoreDisplayX = { 190, 200, 190, 200, 190, 200 }; // first
+																				// one
+																				// only
+	private final double[] ScoreDisplayY = { 20, 20, 30, 30, 40, 40 };
 
-    public void startGame() {
-        restartTime=System.currentTimeMillis();
-        entities.clear();
-        entities.add(new Player(screenX / 2, screenY / 2, 0, 0, this));
-        addRandomAsteroid(4);
-        addRandomUFO();
-        score=0;
-    }
-    
-    public void addRandomUFO(){
-        UFO newUFO = new UFO(((int)(Math.random()*2))*screenX,(float)Math.random()*screenY,0,0,this);
-        if(Math.random()<.5){
-            newUFO.setRadius(5);
-        }
-        create(newUFO);
-        
-    }
-    
-    public void addRandomAsteroid(int times){
-        for (int i = 0; i < times; i++) {
-            entities.add(new Asteroid(0, screenY * (float) Math.random(), (float) Math.random() * 4 - 2,
-                (float) Math.random() * 4 - 2, this));
-        }
-    }
-    
-    public void addAsteroid(float X, float Y, float dX, float dY, float radius){
-        Asteroid newAsteroid = new Asteroid(X, Y, dX, dY, this);
-        newAsteroid.setRadius(radius);
-        addList.add(newAsteroid);
-    }
+	private final int[][] NumberLines = { { 0, 1, 5, 4, 0 }, { 1, 5 }, { 0, 1, 3, 2, 4, 5 }, { 0, 1, 3, 2, 3, 5, 4 },
+			{ 0, 2, 3, 1, 5 }, { 1, 0, 2, 3, 5, 4 }, { 1, 0, 4, 5, 3, 2 }, { 0, 1, 5 }, { 2, 0, 1, 5, 4, 2, 3 },
+			{ 4, 5, 1, 0, 2, 3 } };
 
-    public void update(ArrayList<String> input) {
-        if (input.contains("R")&&System.currentTimeMillis()-restartTime>300){
-            startGame();
-        }
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, screenX, screenY);
-        for (Entity e : entities) {
-            e.update(input);
-            checkCollision(e);
-            e.draw(gc);
-        }
-        entities.removeAll(destroyList);
-        entities.addAll(createList);
-        
-        for (Entity destroyEntity : destroyList) {
-            entities.remove(destroyEntity);
-        }
-        for (Entity addEntity : addList) {
-            entities.add(addEntity);
-        }
-        addList.clear();
-        destroyList.clear();
-        createList.clear();
-        drawScore(gc);
-    }
+	public Game(GraphicsContext gc) {
+		this.gc = gc;
+		screenX = 500;
+		screenY = 500;
+		entities = new ArrayList<Entity>();
+		addList = new ArrayList<Entity>();
+		destroyList = new ArrayList<Entity>();
+		createList = new ArrayList<Entity>();
+		startGame();
+	}
 
-    public void checkCollision(Entity e1){
-        for (Entity e2 : entities) {
-            if (!e1.equals(e2) && Entity.collision(e1, e2) && !destroyList.contains(e1) && !destroyList.contains(e2)){
-                e1.collide(e2);
-            }
-        }
-    }
-    
-    public void drawScore(GraphicsContext gc) {
-        gc.setStroke(Color.WHITE);
-        gc.setLineWidth(2);
-        int rest=score;
-        int digit;
-        for (int i = 1; rest!=0; i++) {
-            digit=(int) (rest%10);
-            rest=(rest-digit)/10;
-            drawDigit(gc,digit,i);
-        }
-    }
+	public void startGame() {
+		restartTime = System.currentTimeMillis();
+		entities.clear();
+		entities.add(new Player(screenX / 2, screenY / 2, 0, 0, this));
+		addRandomAsteroid(4);
+		addRandomUFO();
+		score = 0;
+	}
 
-    private void drawDigit(GraphicsContext gc, int digit, int ofset) {
-        int l=NumberLines[digit].length;
-        double[] scoreX = new double[l];
-        double[] scoreY = new double[l];
-        for (int i = 0; i < l; i++) {
-            scoreX[i]=ScoreDisplayX[NumberLines[digit][i]]-ofset*20;
-            scoreY[i]=ScoreDisplayY[NumberLines[digit][i]];
-        }
-        gc.strokePolyline(scoreX, scoreY, l);
-    }
+	public void addRandomUFO() {
+		UFO newUFO = new UFO(((int) (Math.random() * 2)) * screenX, (float) Math.random() * screenY, 0, 0, this);
+		if (Math.random() < .5) {
+			newUFO.setRadius(5);
+		}
+		create(newUFO);
 
-    public void destroy(Entity e) {
-        destroyList.add(e);
-    }
-    
-    public void create(Entity e) {
-        createList.add(e);
-    }
-    
-    public ArrayList<Entity> getEntities(){
-        return entities;
-    }
+	}
 
-    public float getScreenX() {
-        return screenX;
-    }
+	public void addRandomAsteroid(int times) {
+		for (int i = 0; i < times; i++) {
+			entities.add(new Asteroid(0, screenY * (float) Math.random(), (float) Math.random() * 4 - 2,
+					(float) Math.random() * 4 - 2, this));
+		}
+	}
 
-    public float getScreenY() {
-        return screenY;
-    }
+	public void addAsteroid(float X, float Y, float dX, float dY, float radius) {
+		Asteroid newAsteroid = new Asteroid(X, Y, dX, dY, this);
+		newAsteroid.setRadius(radius);
+		addList.add(newAsteroid);
+	}
 
-    public void over() {
-        for (Entity entity : entities) {
-            if (entity instanceof Player){
-                destroy(entity);
-            }
-        }
-    }
+	public void update(ArrayList<String> input) {
+		if (input.contains("R") && System.currentTimeMillis() - restartTime > 300) {
+			startGame();
+		}
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, screenX, screenY);
+		for (Entity e : entities) {
+			e.update(input);
+			checkCollision(e);
+			e.draw(gc);
+		}
+		entities.removeAll(destroyList);
+		entities.addAll(createList);
 
-    public void addScore(int score) {
-        this.score+=score;
-    }
+		for (Entity destroyEntity : destroyList) {
+			entities.remove(destroyEntity);
+		}
+		for (Entity addEntity : addList) {
+			entities.add(addEntity);
+		}
+		addList.clear();
+		destroyList.clear();
+		createList.clear();
+		drawScore(gc);
+	}
+
+	public void checkCollision(Entity e1) {
+		for (Entity e2 : entities) {
+			if (!e1.equals(e2) && Entity.collision(e1, e2) && !destroyList.contains(e1) && !destroyList.contains(e2)) {
+				e1.collide(e2);
+			}
+		}
+	}
+
+	public void drawScore(GraphicsContext gc) {
+		gc.setStroke(Color.WHITE);
+		gc.setLineWidth(2);
+		int rest = score;
+		int digit;
+		for (int i = 1; rest != 0; i++) {
+			digit = (int) (rest % 10);
+			rest = (rest - digit) / 10;
+			drawDigit(gc, digit, i);
+		}
+	}
+
+	private void drawDigit(GraphicsContext gc, int digit, int ofset) {
+		int l = NumberLines[digit].length;
+		double[] scoreX = new double[l];
+		double[] scoreY = new double[l];
+		for (int i = 0; i < l; i++) {
+			scoreX[i] = ScoreDisplayX[NumberLines[digit][i]] - ofset * 20;
+			scoreY[i] = ScoreDisplayY[NumberLines[digit][i]];
+		}
+		gc.strokePolyline(scoreX, scoreY, l);
+	}
+
+	public void destroy(Entity e) {
+		destroyList.add(e);
+	}
+
+	public void create(Entity e) {
+		createList.add(e);
+	}
+
+	public ArrayList<Entity> getEntities() {
+		return entities;
+	}
+
+	public float getScreenX() {
+		return screenX;
+	}
+
+	public float getScreenY() {
+		return screenY;
+	}
+
+	public void over() {
+		for (Entity entity : entities) {
+			if (entity instanceof Player) {
+				destroy(entity);
+			}
+		}
+	}
+
+	public void addScore(int score) {
+		this.score += score;
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -4,239 +4,243 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Player extends Entity {
-  private int lives;
-  private double rotation;
-  private long lastShot;
+	private int lives;
+	private double rotation;
+	private long lastShot;
 
-  private long invincibleStart;
-  private int invincibleMS;
-  private long hyperspaceStart;
-  private boolean boost;
+	private long invincibleStart;
+	private int invincibleMS;
+	private long hyperspaceStart;
+	private boolean boost;
 
-  /**
-   * Constructor for the Player class.
-   * 
-   * @param X
-   *          location of Player along the X-axis.
-   * @param Y
-   *          location of Player along the Y-axis.
-   * @param dX
-   *          velocity of Player along the X-axis.
-   * @param dY
-   *          velocity of Player along the Y-axis.
-   * @param thisGame
-   *          Game the Player exists in.
-   */
-  public Player(float X, float Y, float dX, float dY, Game thisGame) {
-    super(X, Y, dX, dY, thisGame);
-    lives = 3;
-    radius = 5;
-    rotation = 0;
-    invincibleStart(500);
-  }
+	/**
+	 * Constructor for the Player class.
+	 * 
+	 * @param X
+	 *            location of Player along the X-axis.
+	 * @param Y
+	 *            location of Player along the Y-axis.
+	 * @param dX
+	 *            velocity of Player along the X-axis.
+	 * @param dY
+	 *            velocity of Player along the Y-axis.
+	 * @param thisGame
+	 *            Game the Player exists in.
+	 */
+	public Player(float X, float Y, float dX, float dY, Game thisGame) {
+		super(X, Y, dX, dY, thisGame);
+		lives = 3;
+		radius = 5;
+		rotation = 0;
+		invincibleStart(500);
+	}
 
-  /**
-   * Perform actions that happen when a player dies.
-   */
-  public void die() {
-    lives--;
-    if (lives == 0) {
-      thisGame.over();
-      invincibleStart(500);
-    } else {
-      X = thisGame.getScreenX() / 2;
-      Y = thisGame.getScreenY() / 2;
-      dX = 0;
-      dY = 0;
-      rotation = 0;
-      invincibleStart(500);
-    }
-  }
+	/**
+	 * Perform actions that happen when a player dies.
+	 */
+	public void die() {
+		lives--;
+		if (lives == 0) {
+			thisGame.over();
+			invincibleStart(500);
+		} else {
+			X = thisGame.getScreenX() / 2;
+			Y = thisGame.getScreenY() / 2;
+			dX = 0;
+			dY = 0;
+			rotation = 0;
+			invincibleStart(500);
+		}
+	}
 
-  @Override
-  public void update(ArrayList<String> input) {
-    X = X + dX;
-    Y = Y + dY;
-    slowDown();
-    wrapAround();
-    if (!invincible()) {
-      keyHandler(input);
-    }
-  }
+	@Override
+	public void update(ArrayList<String> input) {
+		X = X + dX;
+		Y = Y + dY;
+		slowDown();
+		wrapAround();
+		if (!invincible()) {
+			keyHandler(input);
+		}
+	}
 
-  /**
-   * Method that translates keyboard input into player character movement.
-   * 
-   * @param input
-   *          arraylist containing the keyboard input
-   */
-  public void keyHandler(ArrayList<String> input) {
-    if (input.contains("LEFT") && !input.contains("RIGHT")) {
-      turnLeft();
-    }
+	/**
+	 * Method that translates keyboard input into player character movement.
+	 * 
+	 * @param input
+	 *            arraylist containing the keyboard input
+	 */
+	public void keyHandler(ArrayList<String> input) {
+		if (input.contains("LEFT") && !input.contains("RIGHT")) {
+			turnLeft();
+		}
 
-    if (input.contains("RIGHT") && !input.contains("LEFT")) {
-      turnRight();
-    }
+		if (input.contains("RIGHT") && !input.contains("LEFT")) {
+			turnRight();
+		}
 
-    if (input.contains("UP")) {
-      accelerate();
-    }
+		if (input.contains("UP")) {
+			accelerate();
+		}
 
-    if (input.contains("DOWN")) {
-      goHyperspace();
-    }
+		if (input.contains("DOWN")) {
+			goHyperspace();
+		}
 
-    if (input.contains("SPACE")) {
-      fire();
-    }
-  }
+		if (input.contains("SPACE")) {
+			fire();
+		}
+	}
 
-  /**
-   * Turn the player left.
-   */
-  private void turnLeft() {
-    rotation += .1;
-  }
+	/**
+	 * Turn the player left.
+	 */
+	private void turnLeft() {
+		rotation += .1;
+	}
 
-  /**
-   * Turn the player right.
-   */
-  private void turnRight() {
-    rotation -= .1;
-  }
+	/**
+	 * Turn the player right.
+	 */
+	private void turnRight() {
+		rotation -= .1;
+	}
 
-  /**
-   * Makes player move faster.
-   */
-  private void accelerate() {
-    dX += Math.cos(rotation) / 10;
-    dY -= Math.sin(rotation) / 10;
-    boost = true;
-  }
+	/**
+	 * Makes player move faster.
+	 */
+	private void accelerate() {
+		dX += Math.cos(rotation) / 10;
+		dY -= Math.sin(rotation) / 10;
+		boost = true;
+	}
 
-  /**
-   * Makes player move slower.
-   */
-  private void slowDown() {
-    if (Math.abs(dX) + Math.abs(dY) != 0) {
-      dX -= (.02 * dX) / (Math.abs(dX) + Math.abs(dY));
-      dY -= (.02 * dY) / (Math.abs(dX) + Math.abs(dY));
-    }
-  }
+	/**
+	 * Makes player move slower.
+	 */
+	private void slowDown() {
+		if (Math.abs(dX) + Math.abs(dY) != 0) {
+			dX -= (.02 * dX) / (Math.abs(dX) + Math.abs(dY));
+			dY -= (.02 * dY) / (Math.abs(dX) + Math.abs(dY));
+		}
+	}
 
-  /**
-   * Starts invincibility that lasts the given amount of milliseconds.
-   * @param milliseconds amount of milliseconds the player should stay invicible.
-   */
-  private void invincibleStart(int milliseconds) {
-    invincibleStart = System.currentTimeMillis();
-    invincibleMS = milliseconds;
-  }
+	/**
+	 * Starts invincibility that lasts the given amount of milliseconds.
+	 * 
+	 * @param milliseconds
+	 *            amount of milliseconds the player should stay invicible.
+	 */
+	private void invincibleStart(int milliseconds) {
+		invincibleStart = System.currentTimeMillis();
+		invincibleMS = milliseconds;
+	}
 
-  /**
-   * @return whether or not the player is invincible at this moment.
-   */
-  public boolean invincible() {
-    return (invincibleStart + invincibleMS > System.currentTimeMillis());
-  }
+	/**
+	 * @return whether or not the player is invincible at this moment.
+	 */
+	public boolean invincible() {
+		return (invincibleStart + invincibleMS > System.currentTimeMillis());
+	}
 
-  /**
-   * Method to handle hyperspace mechanic.
-   */
-  private void goHyperspace() {
-    X = (float) (thisGame.getScreenX() * Math.random());
-    Y = (float) (thisGame.getScreenY() * Math.random());
-    dX = 0;
-    dY = 0;
-    invincibleStart(2000);
-    hyperspaceStart = System.currentTimeMillis();
-  }
+	/**
+	 * Method to handle hyperspace mechanic.
+	 */
+	private void goHyperspace() {
+		X = (float) (thisGame.getScreenX() * Math.random());
+		Y = (float) (thisGame.getScreenY() * Math.random());
+		dX = 0;
+		dY = 0;
+		invincibleStart(2000);
+		hyperspaceStart = System.currentTimeMillis();
+	}
 
-  /**
-   * @return whether or not the player is in hyperspace at this moment.
-   */
-  private boolean hyperspace() {
-    return hyperspaceStart + invincibleMS > System.currentTimeMillis();
-  }
+	/**
+	 * @return whether or not the player is in hyperspace at this moment.
+	 */
+	private boolean hyperspace() {
+		return hyperspaceStart + invincibleMS > System.currentTimeMillis();
+	}
 
-  /**
-   * Method to handle firing bullets.
-   */
-  private void fire() {
-    if (System.currentTimeMillis() - lastShot > 200) {
-      Bullet b = new Bullet(X, Y, dX + ((float) Math.sin(rotation + Math.PI / 2)) * 10,
-          dY + ((float) Math.cos(rotation + Math.PI / 2)) * 10, thisGame);
-      thisGame.create(b);
-      lastShot = System.currentTimeMillis();
-    }
-  }
+	/**
+	 * Method to handle firing bullets.
+	 */
+	private void fire() {
+		if (System.currentTimeMillis() - lastShot > 200) {
+			Bullet b = new Bullet(X, Y, dX + ((float) Math.sin(rotation + Math.PI / 2)) * 10,
+					dY + ((float) Math.cos(rotation + Math.PI / 2)) * 10, thisGame);
+			thisGame.create(b);
+			lastShot = System.currentTimeMillis();
+		}
+	}
 
-  /**
-   * Method to handle collisions of entities with the player.
-   */
-  public void collide(Entity e2) {
-    if (e2 instanceof Asteroid) {
-      if (invincible() && !hyperspace()) {
-        invincibleStart = System.currentTimeMillis();
-      } else if (!invincible()) {
-        ((Asteroid) e2).split();
-        this.die();
-      }
-    } else if (e2 instanceof Bullet && !((Bullet) e2).getFriendly()) {
-      thisGame.destroy(e2);
-      this.die();
-    }
-  }
+	/**
+	 * Method to handle collisions of entities with the player.
+	 */
+	public void collide(Entity e2) {
+		if (e2 instanceof Asteroid) {
+			if (invincible() && !hyperspace()) {
+				invincibleStart = System.currentTimeMillis();
+			} else if (!invincible()) {
+				((Asteroid) e2).split();
+				this.die();
+			}
+		} else if (e2 instanceof Bullet && !((Bullet) e2).getFriendly()) {
+			thisGame.destroy(e2);
+			this.die();
+		}
+	}
 
-  @Override
-  public void draw(GraphicsContext gc) {
-    drawLives(gc);
+	@Override
+	public void draw(GraphicsContext gc) {
+		drawLives(gc);
 
-    double s1 = Math.sin(rotation);
-    double c1 = Math.cos(rotation);
+		double s1 = Math.sin(rotation);
+		double c1 = Math.cos(rotation);
 
-    double s2 = Math.sin(rotation + (Math.PI * 3 / 4));
-    double c2 = Math.cos(rotation + (Math.PI * 3 / 4));
+		double s2 = Math.sin(rotation + (Math.PI * 3 / 4));
+		double c2 = Math.cos(rotation + (Math.PI * 3 / 4));
 
-    double s3 = Math.sin(rotation + (Math.PI * 5 / 4));
-    double c3 = Math.cos(rotation + (Math.PI * 5 / 4));
+		double s3 = Math.sin(rotation + (Math.PI * 5 / 4));
+		double c3 = Math.cos(rotation + (Math.PI * 5 / 4));
 
-    gc.setStroke(Color.WHITE);
-    if (invincible() && (System.currentTimeMillis() + invincibleMS) % 500 < 250) {
-      gc.setStroke(Color.GREY);
-    }
-    if (hyperspace()) {
-      gc.setStroke(Color.BLACK);
-    }
-    gc.setLineWidth(2);
-    gc.strokePolygon(new double[] { X + 10 * c1, X + 10 * c2, X + 10 * c3 },
-        new double[] { Y - 10 * s1, Y - 10 * s2, Y - 10 * s3 }, 3);
+		gc.setStroke(Color.WHITE);
+		if (invincible() && (System.currentTimeMillis() + invincibleMS) % 500 < 250) {
+			gc.setStroke(Color.GREY);
+		}
+		if (hyperspace()) {
+			gc.setStroke(Color.BLACK);
+		}
+		gc.setLineWidth(2);
+		gc.strokePolygon(new double[] { X + 10 * c1, X + 10 * c2, X + 10 * c3 },
+				new double[] { Y - 10 * s1, Y - 10 * s2, Y - 10 * s3 }, 3);
 
-    if (boost) {
-      double s4 = Math.sin(rotation + (Math.PI * 7 / 8));
-      double c4 = Math.cos(rotation + (Math.PI * 7 / 8));
+		if (boost) {
+			double s4 = Math.sin(rotation + (Math.PI * 7 / 8));
+			double c4 = Math.cos(rotation + (Math.PI * 7 / 8));
 
-      double s5 = Math.sin(rotation + (Math.PI * 9 / 8));
-      double c5 = Math.cos(rotation + (Math.PI * 9 / 8));
+			double s5 = Math.sin(rotation + (Math.PI * 9 / 8));
+			double c5 = Math.cos(rotation + (Math.PI * 9 / 8));
 
-      double s6 = Math.sin(rotation + (Math.PI));
-      double c6 = Math.cos(rotation + (Math.PI));
-      gc.strokePolygon(new double[] { X + 9 * c4, X + 9 * c5, X + 12 * c6 },
-          new double[] { Y - 9 * s4, Y - 9 * s5, Y - 12 * s6 }, 3);
-      boost = false;
-    }
-  }
+			double s6 = Math.sin(rotation + (Math.PI));
+			double c6 = Math.cos(rotation + (Math.PI));
+			gc.strokePolygon(new double[] { X + 9 * c4, X + 9 * c5, X + 12 * c6 },
+					new double[] { Y - 9 * s4, Y - 9 * s5, Y - 12 * s6 }, 3);
+			boost = false;
+		}
+	}
 
-  /**
-   * Method to help display the amount of lives the player has left.
-   * @param gc graphicscontext
-   */
-  private void drawLives(GraphicsContext gc) {
-    for (int i = 0; i < lives; i++) {
-      gc.setStroke(Color.WHITE);
-      gc.setLineWidth(2);
-      gc.strokePolygon(new double[] { 10 + 10 * i, 8 + 10 * i, 12 + 10 * i }, new double[] { 10, 18, 18 }, 3);
-    }
-  }
+	/**
+	 * Method to help display the amount of lives the player has left.
+	 * 
+	 * @param gc
+	 *            graphicscontext
+	 */
+	private void drawLives(GraphicsContext gc) {
+		for (int i = 0; i < lives; i++) {
+			gc.setStroke(Color.WHITE);
+			gc.setLineWidth(2);
+			gc.strokePolygon(new double[] { 10 + 10 * i, 8 + 10 * i, 12 + 10 * i }, new double[] { 10, 18, 18 }, 3);
+		}
+	}
 }
