527315447bd0403d43ff3bff868c290c7ca50317
Esmee Hildebrand <e.a.hildebrand@student.tudelft.nl>
2016-09-09 21:55:56 +0200

    Decided some noises that should be in the game.

--- src/Audio.java
+++ src/Audio.java
@@ -1,4 +1,21 @@
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 
+/**
+ * Class to regulate all audio output.
+ * 
+ * @author Esmee
+ *
+ */
 public class Audio {
-
+	// ship noises
+	private AudioClip flyingnoise;
+	private AudioClip shootingnoise;
+	private AudioClip deathnoise;
+	// asteroid noises
+	private AudioClip smallexplosion;
+	private AudioClip mediumexplosion;
+	private AudioClip largeexplosion;
+	// background beat, longer sound so mediaplayer instead of audioclip
+	private MediaPlayer background;
 }
