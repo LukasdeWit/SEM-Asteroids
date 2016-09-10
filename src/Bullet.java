7edc40ff9b3367a91a085802e452f8e7ca6bd61e
Kibo Reinders <ksreinders@gmail.com>
2016-09-10 21:51:42 +0200

    Bullet Checkstyle

--- src/Bullet.java
+++ src/Bullet.java
@@ -7,15 +7,26 @@ import javafx.scene.paint.Color;
  * Class that stores the information for a bullet.
  */
 public class Bullet extends Entity {
+	/**
+	 * Time of creation.
+	 */
 	private long birthTime;
+	/**
+	 * true if this bullet is shot by the player, 
+	 * false if it can hit the player.
+	 */
 	private boolean friendly;
+	/**
+	 * Lifetime of a bullet in miliseconds.
+	 */
+	private static final long LIFETIME = 0;
 
 	/**
 	 * Constructor for the bullet class.
 	 * 
-	 * @param X
+	 * @param x
 	 *            position of bullet along the x-axis
-	 * @param Y
+	 * @param y
 	 *            position of bullet along the y-axis
 	 * @param dX
 	 *            velocity of bullet along the x-axis
@@ -24,8 +35,9 @@ public class Bullet extends Entity {
 	 * @param thisGame
 	 *            game the bullet exists in
 	 */
-	public Bullet(float X, float Y, float dX, float dY, Game thisGame) {
-		super(X, Y, dX, dY, thisGame);
+	public Bullet(final float x, final float y, 
+			final float dX, final float dY, final Game thisGame) {
+		super(x, y, dX, dY, thisGame);
 		setRadius(2);
 		birthTime = System.currentTimeMillis();
 		friendly = true;
@@ -33,12 +45,13 @@ public class Bullet extends Entity {
 
 	/**
 	 * Calculate new position of Bullet.
+	 * @param input - the pressed keys
 	 */
-	public void update(ArrayList<String> input) {
+	public final void update(final ArrayList<String> input) {
 		setX(getX() + getDX());
 		setY(getY() + getDY());
 		wrapAround();
-		if (System.currentTimeMillis() - birthTime > 1000) {
+		if (System.currentTimeMillis() - birthTime > LIFETIME) {
 			getThisGame().destroy(this);
 		}
 	}
@@ -47,15 +60,15 @@ public class Bullet extends Entity {
 	 * Get whether the bullet is friendly.
 	 * @return boolean that is true when bullet is friendly
 	 */
-	public boolean getFriendly() {
+	public final boolean getFriendly() {
 		return friendly;
 	}
 
 	/**
-	 * Set whether the bullet is friendly
+	 * Set whether the bullet is friendly.
 	 * @param friendly value that is true when the bullet is friendly
 	 */
-	public void setFriendly(boolean friendly) {
+	public final void setFriendly(final boolean friendly) {
 		this.friendly = friendly;
 	}
 
@@ -63,7 +76,7 @@ public class Bullet extends Entity {
 	 * Describes what happens when the bullet collides with entities.
 	 */
 	@Override
-	public void collide(Entity e2) {
+	public final void collide(final Entity e2) {
 		if (e2 instanceof Asteroid) {
 			getThisGame().destroy(this);
 			((Asteroid) e2).split();
@@ -71,9 +84,12 @@ public class Bullet extends Entity {
 	}
 
 	@Override
-	public void draw(GraphicsContext gc) {
+	public final void draw(final GraphicsContext gc) {
 		float radius = getRadius();
 		gc.setFill(Color.WHITE);
-		gc.fillOval(getX() - radius / 2, getY() - radius / 2, radius * 2, radius * 2);
+		gc.fillOval(getX() - radius / 2, 
+				getY() - radius / 2, 
+				radius * 2, 
+				radius * 2);
 	}
 }
