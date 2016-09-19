d9cf490d4b91dbb9d325c933d58da3e4e65c5cc1
Dario Hartog <dhartog@student.tudelft.nl>
2016-09-19 09:59:14 +0200

    De asteroidTest in entity werkt niet; en die in src ook niet.

---
+++ src/BulletTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BulletTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBullet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFriendly() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFriendly() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/PlayerTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCollide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInvincible() {
+		fail("Not yet implemented");
+	}
+
+}
