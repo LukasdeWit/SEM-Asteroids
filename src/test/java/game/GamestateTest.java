4d381953d1349e9a93d0f36c923de7dec65c8aba
Esmee Hildebrand <e.a.hildebrand@student.tudelft.nl>
2016-10-17 15:46:21 +0200

    Fixed game state test

--- src/test/java/game/GamestateTest.java
+++ src/test/java/game/GamestateTest.java
@@ -118,8 +118,8 @@ public class GamestateTest {
 		gamestate.setState(Gamestate.getStateHighscoreScreen());
 		input.add("R");
 		gamestate.update(input);
-		assertEquals(Gamestate.getModeArcade(), gamestate.getMode(), 1);
-		assertEquals(Gamestate.getStateHighscoreScreen(), gamestate.getState(), 2);
+		assertEquals(Gamestate.getModeNone(), gamestate.getMode(), 0);
+		assertEquals(Gamestate.getStateStartScreen(), gamestate.getState(), 0);
 	}
 	
 	@Test
